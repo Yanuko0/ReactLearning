62db5ce8bf03d1e323374c177cff3da0eda32140
立瑜 <baredragon0@gmail.com>
2024-07-10 14:24:18 +0800

    整體路由設計配置

--- react-bill-test/src/index.js
+++ react-bill-test/src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import sum from "@/test"
 
-const total = sum(1,3)
-console.log(total)
+// const total = sum(1,3)
+// console.log(total)
+import {RouterProvider} from 'react-router-dom'
+import router  from './router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // 嚴格模式的節點,造成某些代碼執行兩次
   // <React.StrictMode>
-    <App />
+    
   // {/* </React.StrictMode> */}
+  <RouterProvider router={router}/>
+
+ 
 );
 
 
