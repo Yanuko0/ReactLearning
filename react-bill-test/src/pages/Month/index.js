e82f8046a9f998c069e0c8171e656c84bd255451
立瑜 <baredragon0@gmail.com>
2024-07-12 15:11:40 +0800

    月度帳單-切換彈框功能

--- react-bill-test/src/pages/Month/index.js
+++ react-bill-test/src/pages/Month/index.js
@@ -1,7 +1,17 @@
 import { NavBar, DatePicker } from "antd-mobile";
 import './index.scss'
+import { useState } from "react";
+import classNames from "classnames";
 
 const Month = () => {
+    //控制彈框的打開和關閉
+    const [ dateVisible , setDateVisible] = useState(false)
+
+    const onConfirm = () =>{
+        setDateVisible(false)
+        //其他邏輯
+    }
+
     return (
         <div className="monthlyBill">
             <NavBar className="nav" backArrow={false}>
@@ -10,11 +20,12 @@ const Month = () => {
             <div className="content">
                 <div className="header">
                     {/* 時間切換區域 */}
-                    <div className="date">
+                    <div className="date" onClick={()=>{setDateVisible(true)}}>
                         <sapn className="text">
                             2023 | 3月帳單
                         </sapn>
-                        <span className="arrow expand"></span>
+                        {/* 思路:控制當前彈框打開的狀態 控制expand類名是否存在 */}
+                        <span className={classNames('arrow',dateVisible && 'expand')}></span>
                     </div>
                     {/* 統計區域 */}
                     <div className="twoLineOverview">
@@ -35,8 +46,12 @@ const Month = () => {
                         className="kaDate"
                         title="記帳日期"
                         precision="month"
-                        visible={false}
-                        max={new Date()}/>
+                        visible={dateVisible}
+                        max={new Date()}
+                        onCancel={()=>setDateVisible(false)}
+                        onConfirm={onConfirm}
+                        onClose={()=>setDateVisible(false)}
+                        />
                 </div>
             </div>
         </div>
