89fed028127eb07530d45cae1fb835cc1fc150eb
立瑜 <baredragon0@gmail.com>
2024-07-26 09:56:36 +0800

    登錄-axios封裝處理

---
+++ 建置CRA項目/react-jike/src/utils/index.js
@@ -0,0 +1,8 @@
+//統一中轉工具模塊函數
+// import { request } from '@/utils'
+
+import { request } from "./request";
+
+export {
+    request
+}
--- 建置CRA項目/react-jike/src/utils/request.js
+++ 建置CRA項目/react-jike/src/utils/request.js
@@ -9,4 +9,25 @@ const request = axios.create({
     timeout: 5000  //超時時間5秒鐘
 })
 
-export {request}
+//添加請求攔截器
+//請求發送之前 做攔截 插入自定義的配置[參數的處理]
+request.interceptors.request.use((config)=>{
+    return config
+},(error)=>{
+    return Promise.reject(error)
+})
+
+//添加響應攔截器
+//在響應返回到客戶端之前 做攔截 重點處理返回的數據
+request.interceptors.response.use((response)=>{
+    //2xx範圍內的狀態碼都會觸發該函數
+    //對響應數據做點什麼
+    return response.data
+},(error)=>{
+    //2xx範圍內的狀態碼都會觸發該函數
+    //對響應錯誤做點什麼
+    return Promise.reject(error)
+})
+
+
+export { request }
