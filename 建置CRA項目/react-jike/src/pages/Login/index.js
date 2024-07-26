01154c6c4c6930953cb33cf17cef974f3520f010
立瑜 <baredragon0@gmail.com>
2024-07-26 13:51:18 +0800

    使用redux維護token數據

--- 建置CRA項目/react-jike/src/pages/Login/index.js
+++ 建置CRA項目/react-jike/src/pages/Login/index.js
@@ -1,11 +1,22 @@
 import "./index.scss"
-import {Card, Form, Input, Button} from "antd"
+import {Card, Form, Input, Button, message} from "antd"
 import logo from "@/assets/logo.png"
 import { values } from "lodash"
+import { useDispatch } from "react-redux"
+import { fetchLogin } from "@/store/modules/user"
+import { useNavigate } from "react-router-dom"
 
 const Login = () =>{
-    const onFinish = (values) => {
+    const dispatch = useDispatch()
+    const navigat =  useNavigate()
+    const onFinish = async(values) => {
         console.log(values)
+        //觸發異步action fetchLogin
+        await dispatch(fetchLogin(values))
+        //1.跳轉到首頁
+        navigat('/')
+        //2.提示用戶是否登錄成功
+        message.success('登入成功')
     }
     return(
         <div className="login">
@@ -22,7 +33,8 @@ const Login = () =>{
                         message: '請輸入手機號!',
                       },
                       {
-                        pattern:/^(?:\+?886\-?|0)?9\d{8}$/,
+                        // pattern:/^(?:\+?886\-?|0)?9\d{8}$/,
+                        pattern: /^1[3-9]\d{9}$/,
                         message: '請輸入正確的手機號格式!',
                       }
                     ]}>
---
+++ 建置CRA項目/react-jike/src/store/modules/user.js
@@ -0,0 +1,41 @@
+//和用戶相關的狀態管理
+
+import { createSlice } from "@reduxjs/toolkit";
+import { request } from "@/utils";
+
+const userStore = createSlice({
+    name: "user",
+    //數據狀態
+    initialState:{
+        token: ''
+    },
+    //同步的修改方法
+    reducers:{
+        setToken(state, action) {
+            state.token = action.payload
+        }
+    }
+})
+
+//解構出actionCreater
+
+const { setToken } = userStore.actions
+
+//獲取reducer函數
+
+const userReducer = userStore.reducer
+
+//異步方法 完成登錄獲取token
+
+const fetchLogin = ( loginForm ) =>{
+    return async(dispatch)=>{
+        //1.發送異步請求
+        const res = await request.post('/authorizations', loginForm)
+        //2.提交同步action進行token的存入
+        dispatch(setToken(res.data.token))
+    }
+}
+
+export { fetchLogin, setToken }
+
+export default userReducer
