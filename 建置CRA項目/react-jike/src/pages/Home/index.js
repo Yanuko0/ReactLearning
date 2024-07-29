49c3503a8d15d7f0ab7f6e28d3bb1a9f0bc51ed6
立瑜 <baredragon0@gmail.com>
2024-07-29 16:14:30 +0800

    圖表初始化

--- 建置CRA項目/react-jike/src/pages/Home/index.js
+++ 建置CRA項目/react-jike/src/pages/Home/index.js
@@ -1,5 +1,44 @@
+import * as echarts from 'echarts';
+import { useEffect, useRef } from 'react';
+
+
+
+
+
 const Home = () => {
-    return <div>this is home</div>
+    const chartRef = useRef(null)
+    useEffect(() => {
+        // 1.獲取渲染圖表的dom節點
+        // const chartDom = document.getElementById('main');
+        const chartDom = chartRef.current
+        // 2.圖表初始化生成圖表實例對象
+        const myChart = echarts.init(chartDom);
+        // 3.準備圖表參數
+        const option = {
+            xAxis: {
+                type: 'category',
+                data: ['Vue','React','Angular']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    data: [10,40,70],
+                    type: 'bar'
+                }
+            ]
+        };
+        // 4.使用圖表參數完成圖表渲染
+        option && myChart.setOption(option);
+
+    }, [])
+
+    return (
+        <div>
+            <div ref={chartRef} style={{width:"500px", height:"400px"}}></div>
+
+        </div>)
 }
 
 export default Home
