5ab2283eec6634e99f3bf206ee6841f790dd89a4
立瑜 <baredragon0@gmail.com>
2024-07-29 17:23:48 +0800

    發布文章-基礎結構搭建

--- 建置CRA項目/react-jike/src/pages/Publish/index.js
+++ 建置CRA項目/react-jike/src/pages/Publish/index.js
@@ -1,5 +1,68 @@
+import {
+    Card,
+    Breadcrumb,
+    Form,
+    Button,
+    Radio,
+    Input,
+    Upload,
+    Space,
+    Select,
+} from 'antd'
+import { PlusOutlined } from "@ant-design/icons"
+import { Link } from 'react-router-dom'
+import './index.scss'
+
 const Publish = () => {
-    return <div>this is publish</div>
+    return (
+        <div className='publish'>
+            <Card
+                title={
+                    <Breadcrumb items={[
+                        { title: <Link to={'/'}>首頁</Link> },
+                        { title: '發布文章' },
+                    ]}
+                    />
+                }
+            >
+                <Form
+                    labelCol={{ span: 4 }}
+                    wrapperCol={{ span: 16 }}
+                    initialValues={{ type: 1 }}
+                >
+                    <Form.Item
+                        label="標題"
+                        name="title"
+                        rules={[{ required: true, message: '請輸入文章標題' }]}
+                    >
+                        <Input placeholder='請輸入文章標題' style={{ width: 400 }} />
+                    </Form.Item>
+                    <Form.Item
+                        label='頻道'
+                        name="channel_id"
+                        rules={[{ required: true, message: '請選擇文章頻道' }]}
+                    >
+                        <Select placeholder='請選擇文章頻道' style={{ width: 400 }}>
+                            <option value={0}>推薦</option>
+                        </Select>
+                    </Form.Item>
+                    <Form.Item
+                        label='內容'
+                        name="content"
+                        rules={[{ required: true, message: '請輸入文章內容' }]}
+                    >
+                        {/* 富文本編輯器 */}
+                    </Form.Item>
+                    <Form.Item wrapperCol={{ offset: 4 }}>
+                        <Space>
+                            <Button size='large' type='primary' htmlType='submit'>
+                                發布文章</Button>
+                        </Space>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
+    )
 }
 
 export default Publish
